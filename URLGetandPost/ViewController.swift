60847f4beca31a9ae917ad6c0813436c15fe17f7
Karthik Rajan T  <karthikrajant@Karthiks-MacBook-Pro.local>
2020-08-14 15:31:40 +0530

    firstCommit

---
+++ URLGetandPost/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  URLGetandPost
+//
+//  Created by Karthik Rajan T  on 13/08/20.
+//  Copyright © 2020 Karthik Rajan T . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBAction func onGetTapped(_ sender: Any) {
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts?userId=1") else { return }
+        
+        let session = URLSession.shared
+        session.dataTask(with: url) { (data, response, error) in
+            if let response = response {
+                print(response)
+            }
+            
+            if let data = data {
+                print(data)
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                } catch {
+                    print(error)
+                }
+                
+            }
+        }.resume()
+    }
+    
+    
+    @IBAction func onPostTapped(_ sender: Any) {
+        
+        let parameters = ["username": "@karthik", "tweet": "HelloWorld"]
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else { return }
+        request.httpBody = httpBody
+        
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            if let response = response {
+                print(response)
+            }
+            
+            if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                } catch {
+                    print(error)
+                }
+            }
+            
+        }.resume()
+        
+    }
+    
+    
+}
+
